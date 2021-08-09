d6cd10e0f57e94780fadcea5440ef1d3470f4710
Tri Tran <tranminhtri9090@gmail.com>
2021-08-10 00:39:20 +1000

    (fix) Adding requirements to integrate emoji-picker and text-editor

--- src/components/Note/components/EmojiPicker.jsx
+++ src/components/Note/components/EmojiPicker.jsx
@@ -3,6 +3,10 @@ import "emoji-mart/css/emoji-mart.css";
 import { Picker } from "emoji-mart";
 
 const EmojiPicker = ({ content, setContent }) => {
+  const isEmpty = (str) => {
+    return str.replace(/^\s+|\s+$/g, "").length == 0;
+  };
+
   return (
     <Picker
       set="twitter"
@@ -11,7 +15,15 @@ const EmojiPicker = ({ content, setContent }) => {
       showSkinTones={false}
       onSelect={(emoji) => {
         console.log("emoji", emoji);
-        setContent(`${content}${emoji.native}`);
+        console.log("content", content);
+
+        const test = content.match(/<h(.)>.*?<\/h\1>/g);
+        console.log("test", test);
+
+        // Between >{children} [HERE] <
+        // Write a logic to insert an emoji.native into HERE position for the last element in the test array
+
+        setContent(content + emoji.native);
       }}
       style={{ marginTop: "10px", marginBottom: "10px" }}
     />
