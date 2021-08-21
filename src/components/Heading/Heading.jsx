602f9e4395c4b39eceb63ce6a1a3610ed7b54378
Tri Tran <tranminhtri9090@gmail.com>
2021-08-21 12:56:22 +1000

    (feat) Adding delete HEADING button

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -3,7 +3,7 @@ import { Box, Icon } from "@chakra-ui/react";
 import TexInput from "./components/EditInput/Input";
 import { getEditHeading } from "redux/features/heading/heading";
 import { useDispatch, useSelector } from "react-redux";
-import { BiPen } from "react-icons/bi";
+import { BiPen, BiTrash } from "react-icons/bi";
 import "./Heading.scss";
 
 const Heading = ({
@@ -62,9 +62,6 @@ const Heading = ({
           transform={`translate(${positionX}px, ${positionY}px) rotate(${headingRotateDegree}deg)`}
           fontFamily={headingFontFamily}
           color={headingColor}
-          display="flex"
-          justifyContent="flex-start"
-          alignItems="flex-start"
           fontWeight={headingBold ? "bold" : "normal"}
           fontStyle={headingItalic ? "italic" : "initial"}
           textDecoration={
@@ -74,23 +71,25 @@ const Heading = ({
               ? "line-through"
               : "initial"
           }
+          display="flex"
+          justifyContent="flex-start"
+          alignItems="center"
+          gridGap="4"
           className="hoverEffect"
         >
           {input}
-          <Icon
-            as={BiPen}
-            onClick={() => ActiveInput()}
-            width="32px"
-            height="32px"
-            p={1}
-            borderRadius="full"
-            bg="white"
-            color="black"
-            zIndex="5"
-            cursor="pointer"
-            className="editButton"
-            transition="visibility 0s, opacity 0.25s"
-          />
+          <Box display="flex" flexDirection="column" gridGap="4">
+            <Icon
+              as={BiPen}
+              onClick={() => ActiveInput()}
+              className="actionButton"
+            />
+            <Icon
+              as={BiTrash}
+              onClick={() => ActiveInput()}
+              className="actionButton"
+            />
+          </Box>
         </Box>
       )}
     </>
