7825ac92b4109a3f4783d1206714b6e4e551edd4
Tri Tran <tranminhtri9090@gmail.com>
2021-08-24 22:57:10 +1000

    (feat) Building UploadImages UI ( Drag & Drop Zones with Clear all preview Images button

--- src/components/Images/components/Modal/createImageModal.jsx
+++ src/components/Images/components/Modal/createImageModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CommonModal from "components/NoteModal/CommonModal";
 import {
   ModalHeader,
@@ -7,8 +7,10 @@ import {
   ModalFooter,
   Button,
   MenuItem,
+  Box,
+  Icon,
 } from "@chakra-ui/react";
-import { BiImageAdd } from "react-icons/bi";
+import { BiImageAdd, BiUpload, BiTrash } from "react-icons/bi";
 import ImageUploading from "react-images-uploading";
 
 const CreateImagesModal = ({
@@ -18,6 +20,14 @@ const CreateImagesModal = ({
   modalState,
   setModalState,
 }) => {
+  const [images, setImages] = useState([]);
+
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
   };
@@ -43,7 +53,91 @@ const CreateImagesModal = ({
         >
           <ModalHeader>Adding Images</ModalHeader>
           <ModalCloseButton />
-          <ModalBody pb={6}></ModalBody>
+          <ModalBody pb={6}>
+            <ImageUploading
+              multiple
+              value={images}
+              onChange={onChange}
+              dataURLKey="data_url"
+            >
+              {({
+                imageList,
+                onImageUpload,
+                onImageRemoveAll,
+                onImageUpdate,
+                onImageRemove,
+                isDragging,
+                dragProps,
+              }) => (
+                // write your building UI
+                <div className="upload__image-wrapper">
+                  <Box display="flex" gridGap="2">
+                    <Box
+                      style={isDragging ? { color: "red" } : undefined}
+                      onClick={onImageUpload}
+                      width="90%"
+                      height="250px"
+                      color="white"
+                      bg="#2c3333"
+                      cusror="pointer"
+                      display="flex"
+                      justifyContent="center"
+                      {...dragProps}
+                    >
+                      <Box
+                        width="100%"
+                        display="flex"
+                        margin={4}
+                        border="2px dashed white"
+                        justifyContent="center"
+                        alignItems="center"
+                        flexDirection="column"
+                      >
+                        <Icon
+                          as={BiUpload}
+                          color="white"
+                          width="50px"
+                          height="50px"
+                        />
+                        <Box>Choose an Image or drag it here</Box>
+                      </Box>
+                    </Box>
+                    <Box width="10%">
+                      <Box
+                        width="fit-content"
+                        bg="#2c3333"
+                        borderRadius="full"
+                        cursor="pointer"
+                        onClick={onImageRemoveAll}
+                      >
+                        <Icon
+                          as={BiTrash}
+                          color="white"
+                          width="36px"
+                          height="36px"
+                          p={2}
+                        />
+                      </Box>
+                    </Box>
+                  </Box>
+                  &nbsp;
+                  {imageList.map((image, index) => (
+                    <div key={index} className="image-item">
+                      <img src={image["data_url"]} alt="" width="100" />
+                      <div className="image-item__btn-wrapper">
+                        <button onClick={() => onImageUpdate(index)}>
+                          Update
+                        </button>
+                        <button onClick={() => onImageRemove(index)}>
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </ImageUploading>
+          </ModalBody>
 
           <ModalFooter>
             <Button
