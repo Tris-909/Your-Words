121bdf091c831f23c874db6d37b7a8641f32fd76
Tri Tran <tranminhtri9090@gmail.com>
2021-09-04 11:15:03 +1000

    (fix) Only load emojiPicker after editNoteModal has rendered

--- src/components/Note/components/BodyNoteEditor.jsx
+++ src/components/Note/components/BodyNoteEditor.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import TextEditor from "components/Note/components/TextEditor";
 import EmojiPicker from "components/Note/components/EmojiPicker";
 
 const BodyNoteEditor = ({ content, setContent }) => {
+  const [loadingEmoji, setLoadingEmoji] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadingEmoji(true);
+    }, 500);
+  }, []);
+
   return (
     <Grid templateColumns="repeat(5, 1fr)" gap={3} position="relative">
       <GridItem colSpan={3}>
         <TextEditor content={content} setContent={setContent} />
       </GridItem>
-      <GridItem colStart={4} colEnd={6} height="100%">
-        <EmojiPicker content={content} setContent={setContent} />
-      </GridItem>
+      {loadingEmoji && (
+        <GridItem colStart={4} colEnd={6} height="100%">
+          <EmojiPicker content={content} setContent={setContent} />
+        </GridItem>
+      )}
     </Grid>
   );
 };
