b0f5d6f80572b35d4a57a4fa984c175117825902
Tri Tran <tranminhtri9090@gmail.com>
2021-12-19 17:33:00 +1000

    (fix) Fixing Common Image component by replacing old aws-exports for new one

--- src/components/Common/Image/Image.jsx
+++ src/components/Common/Image/Image.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Image } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
-import config from "aws-exports";
+import config from "aws-exports-env";
+const ENV = process.env.REACT_APP_DEV_ENV;
 
 const CommonImage = (props) => {
   const { auth } = useSelector((state) => state.user);
@@ -10,7 +11,7 @@ const CommonImage = (props) => {
     <>
       {auth && auth.data ? (
         <Image
-          src={`https://${config.aws_user_files_s3_bucket}.s3.${config.aws_user_files_s3_bucket_region}.amazonaws.com/private/${auth.data.id}/${props.source}`}
+          src={`https://${config[ENV].aws_user_files_s3_bucket}.s3.${config[ENV].aws_user_files_s3_bucket_region}.amazonaws.com/private/${auth.data.id}/${props.source}`}
           {...props}
         />
       ) : null}
